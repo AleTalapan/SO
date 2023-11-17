435c199dcedea4b7f1712406256be5614907aa9d
Alexandra <aletalapan@gmail.com>
2023-11-17 02:11:20 +0200

    s7

--- proiect.c
+++ proiect.c
@@ -8,21 +8,44 @@
 #include <string.h>
 #include <stdint.h>
 #include <time.h>
+#include <dirent.h>
 
 int file, file2;
 struct stat buff;
 uint32_t file_size;
 uint32_t width;
 uint32_t height;
-char line[50];
+char line[350];
 char u[4];
 char g[4];
 char o[4];
+struct dirent *f;
+char cale[300];
 
 int is_image(char *path, struct stat buff){
  
 
   if(S_ISREG(buff.st_mode) && strstr(path, ".bmp")){
+      return 1;
+  }
+  return 0;
+
+}
+
+int is_dir(char *path, struct stat buff){
+ 
+
+  if(S_ISDIR(buff.st_mode)){
+      return 1;
+  }
+  return 0;
+
+}
+
+int is_reg(char *path, struct stat buff){
+ 
+
+  if(S_ISREG(buff.st_mode)){
       return 1;
   }
   return 0;
@@ -121,16 +144,10 @@ int main(int args, char **argv){
   if(stat(argv[1], &buff) == -1){
     perror("stat not working");
     exit(EXIT_FAILURE);
-  }
+    }
 
-  if(!is_image(argv[1], buff)){
+  if(!is_dir(argv[1], buff)){
     perror("Incorrect file type");
-    exit(EXIT_FAILURE);
-  }
-  
-  file=open(argv[1], O_RDONLY);
-  if(file == -1){
-    perror("Nu s-a putut deschide imaginea");
     exit(EXIT_FAILURE);
   }
 
@@ -140,43 +157,72 @@ int main(int args, char **argv){
     exit(EXIT_FAILURE);
   }
 
-  read_header();
+  DIR* director=opendir(argv[1]);
+  if(director == NULL){
+    perror("Can't open directory");
+    exit(EXIT_FAILURE);
+  }
 
-  uid_t user_id = buff.st_uid;
-  struct timespec lastModifiedTime = buff.st_mtim;
-  struct tm *time = localtime(&lastModifiedTime.tv_sec);
-  nlink_t no_links = buff.st_nlink;
-  mode_t access = buff.st_mode;
+  readdir(director);
+  readdir(director);
+  while((f=readdir(director)) != NULL){
+    sprintf(cale, "%s/%s", argv[1], f->d_name);
+    printf("%s\n", cale);
+    
+    if(stat(cale, &buff) == -1){
+    perror("stat not working");
+    exit(EXIT_FAILURE);
+    }
+    
+    if(is_image(cale,buff)){
+      file=open(cale, O_RDONLY);
+      if(file == -1){
+	 perror("Nu s-a putut deschide imaginea");
+	 exit(EXIT_FAILURE);
+       }
+       read_header();
 
-  sprintf(line, "nume fisier: %s\n", argv[1]);
-  write_file();
+       uid_t user_id = buff.st_uid;
+       struct timespec lastModifiedTime = buff.st_mtim;
+       struct tm *time = localtime(&lastModifiedTime.tv_sec);
+       nlink_t no_links = buff.st_nlink;
+       mode_t access = buff.st_mode;
+
+       sprintf(line, "nume fisier: %s\n", cale);
+       write_file();
   
-  sprintf(line, "inaltime: %d\n", width);
-  write_file();
+       sprintf(line, "inaltime: %d\n", width);
+       write_file();
 
-  sprintf(line, "lungime: %d\n", height);
-  write_file();
+       sprintf(line, "lungime: %d\n", height);
+       write_file();
 
-  sprintf(line, "dimensiune: %d\n", file_size);
-  write_file();
+       sprintf(line, "dimensiune: %d\n", file_size);
+       write_file();
 
-  sprintf(line, "identificatorul utilizatorului: %d\n", user_id);
-  write_file();
+       sprintf(line, "identificatorul utilizatorului: %d\n", user_id);
+       write_file();
 
-  sprintf(line, "timpul ultimei modificari: %d.%d.%d\n", time->tm_mday, time->tm_mon+1, time->tm_year+1900);
-  write_file();
+       sprintf(line, "timpul ultimei modificari: %d.%d.%d\n", time->tm_mday, time->tm_mon+1, time->tm_year+1900);
+       write_file();
 
-  sprintf(line, "contorul de legaturi: %ld\n", no_links);
-  write_file();
+       sprintf(line, "contorul de legaturi: %ld\n", no_links);
+       write_file();
 
-  sprintf(line, "drepturi de acces user: %s\n", access_user(access));
-  write_file();
+       sprintf(line, "drepturi de acces user: %s\n", access_user(access));
+       write_file();
 
-  sprintf(line, "drepturi de acces grup: %s\n", access_group(access));
-  write_file();
+       sprintf(line, "drepturi de acces grup: %s\n", access_group(access));
+       write_file();
 
-  sprintf(line, "drepturi de acces altii: %s\n", access_others(access));
-  write_file();
+       sprintf(line, "drepturi de acces altii: %s\n", access_others(access));
+       write_file();
 
+    }
+  }
+
+ 
+
+  
   
 }
