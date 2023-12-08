031997a3d855ef122d6864f40f440755cee280eb
Alexandra <aletalapan@gmail.com>
2023-12-09 00:15:42 +0200

    s8 final

--- proiect.c
+++ proiect.c
@@ -11,7 +11,7 @@
 #include <dirent.h>
 #include <sys/wait.h>
 
-int file, file2;
+int file, file2, fd;
 struct stat buff;
 uint32_t file_size;
 uint32_t width;
@@ -22,6 +22,7 @@ char g[4];
 char o[4];
 struct dirent *f;
 char cale[300];
+char filepath[300];
 
 int is_image(char *path, struct stat buff){
  
@@ -72,8 +73,8 @@ void read_header(){
 }
 
 void write_file(){
-  if(write(file2, line, strlen(line)) == -1){
-    perror("Nu se scrie");
+  if(write(fd, line, strlen(line)) == -1){
+    perror("Error at writing");
     exit(EXIT_FAILURE);
   }
 }
@@ -141,36 +142,43 @@ char *access_others(mode_t access){
   return o;
 }
 
-void info_files(int reg, int file_size, int user_id, struct tm *time, long int no_links, mode_t access){
+void info_files(int reg, int file_size, int user_id, struct tm *time, long int no_links, mode_t access, int *written){
 
   if(reg){
     sprintf(line, "dimensiune: %d\n", file_size);
     write_file();
+    (*written)++;
   }
 
   sprintf(line, "identificatorul utilizatorului: %d\n", user_id);
   write_file();
+  (*written)++;
 
   if(reg){
     sprintf(line, "timpul ultimei modificari: %d.%d.%d\n", time->tm_mday, time->tm_mon+1, time->tm_year+1900);
     write_file();
+    (*written)++;
 
     sprintf(line, "contorul de legaturi: %ld\n", no_links);
     write_file();
+    (*written)++;
   }
 
   sprintf(line, "drepturi de acces user: %s\n", access_user(access));
   write_file();
+  (*written)++;
 
   sprintf(line, "drepturi de acces grup: %s\n", access_group(access));
   write_file();
+  (*written)++;
 
   sprintf(line, "drepturi de acces altii: %s\n", access_others(access));
   write_file();
+  (*written)++;
 
 }
 
-void info_symlink(char *name, int file_size, mode_t access){
+void info_symlink(char *name, int file_size, mode_t access, int *written){
   struct stat sym_link;
   if(stat(cale, &sym_link) == -1){
     perror("Eroare lstat target");
@@ -179,29 +187,35 @@ void info_symlink(char *name, int file_size, mode_t access){
   
   sprintf(line, "nume legatura: %s\n", name);
   write_file();
+  (*written)++;
 
   sprintf(line, "dimensiune: %d\n", file_size);
   write_file();
+  (*written)++;
 
   sprintf(line, "dimensiune fisier: %ld\n", sym_link.st_size);
   write_file();
+  (*written)++;
 
   sprintf(line, "drepturi de acces user: %s\n", access_user(access));
   write_file();
+  (*written)++;
 
   sprintf(line, "drepturi de acces grup: %s\n", access_group(access));
   write_file();
+  (*written)++;
 
   sprintf(line, "drepturi de acces altii: %s\n", access_others(access));
   write_file();
+  (*written)++;
   
 }
 
 void statistica(char* dir, char* name){
-  char filepath[300];
+
   sprintf(filepath, "%s/%s_statistica.txt", dir, name);
 	
-  int fd = open(filepath, O_RDONLY | O_WRONLY | O_CREAT | O_TRUNC, 0666);
+  fd = open(filepath, O_RDONLY | O_WRONLY | O_CREAT | O_TRUNC, 0666);
   if(fd == -1){
     perror("Can't open file for writing directory");
     exit(EXIT_FAILURE);
@@ -209,16 +223,49 @@ void statistica(char* dir, char* name){
 }
 
 void waiting(pid_t pid){
-  int status;
-  pid_t w = waitpid(pid, &status, 0);
-      
-  if(w == -1){
-    perror("Error waitpid directory");
+      int status;
+      pid_t terminated_pid = waitpid(pid, &status, 0);
+
+      if(terminated_pid == -1){
+	perror("Error at waitpid");
+	exit(EXIT_FAILURE);
+      }
+
+      if(WIFEXITED(status)){
+	int written = WEXITSTATUS(status);
+        sprintf(line, "numar linii %s: %d\n", f->d_name, written);
+	write(file2, line, strlen(line));
+	printf("Process with id %d ended with the status %d\n", pid,status);
+      }
+      else{
+	printf("Process with id %d ended unexpectedly.\n", pid);
+      }
+}
+
+void gray_scale(char *path){
+  int img = open(path, O_RDWR);
+  if(file == -1){
+    perror("Error opening image");
     exit(EXIT_FAILURE);
   }
-  else if(w == pid){
-    printf("Process with id %d ended with the status %d\n", pid,status);
-}
+  char buffer[54];
+  if(read(img, buffer, 54) != 54){
+    perror("Error reading bmp");
+    exit(EXIT_FAILURE);
+  }
+  unsigned char pixels[3];
+  lseek(img, 54, SEEK_SET);
+
+  while(read(img, pixels, 3) == 3){
+    unsigned char p_gri = (unsigned char)(0.299 * pixels[2] + 0.587 * pixels[1] + 0.114 * pixels[0]);
+
+    lseek(img, -3, SEEK_CUR);
+    unsigned char grayPixel[3] = {p_gri, p_gri, p_gri};
+    write(img, grayPixel, 3);
+  }
+
+  close(file);
+  
 }
 
 
@@ -281,34 +328,68 @@ int main(int args, char **argv){
     nlink_t no_links = buff.st_nlink;
     mode_t access = buff.st_mode;
     int reg = 0;
+    int written_lines = 0;
     
     if(is_image(cale,buff)){
-      
       file=open(cale, O_RDONLY);
       if(file == -1){
-	 perror("Nu s-a putut deschide imaginea");
+	 perror("Error opening image");
 	 exit(EXIT_FAILURE);
        }
       
        read_header();
        reg = 1;
 
-       sprintf(line, "nume fisier: %s\n", f->d_name);
-       write_file();
+      pid_t id_img = fork();
+      if(id_img < 0){
+	perror("Error fork image");
+	exit(EXIT_FAILURE);
+      }
+      if(id_img == 0){
+	statistica(argv[2], f->d_name);
+	sprintf(line, "nume fisier: %s\n", f->d_name);
+        write_file();
+	written_lines++;
 
-       sprintf(line, "inaltime: %d\n", width);
-       write_file();
+        sprintf(line, "inaltime: %d\n", width);
+        write_file();
+	written_lines++;
 
-       sprintf(line, "lungime: %d\n", height);
-       write_file();
+        sprintf(line, "lungime: %d\n", height);
+        write_file();
+	written_lines++;
 
-       info_files(reg, file_size, user_id, time, no_links, access);
+	info_files(reg, file_size, user_id, time, no_links, access, &written_lines);
+	close(file);
+	exit(written_lines);
+      }
+      waiting(id_img);
+
+      pid_t id_gray = fork();
+      if(id_gray == -1){
+	perror("Error fork gray scale");
+	exit(EXIT_FAILURE);
+      }
+
+      if(id_gray == 0){
+	gray_scale(cale);
+	exit(EXIT_SUCCESS);
+      }
+      int status;
+      pid_t terminated_pid = waitpid(id_gray, &status, 0);
+      if(terminated_pid == -1){
+	perror("Error at waitpid");
+	exit(EXIT_FAILURE);
+      }
+      if(terminated_pid == id_gray){
+	printf("Process with id %d ended with the status %d\n", id_gray,status);
+      }
 
     }
     else if(is_reg(buff)){
       pid_t id_reg = fork();
       if(id_reg < 0){
-	perror("Error fork directory");
+	perror("Error fork regular file");
 	exit(EXIT_FAILURE);
       }
       if(id_reg == 0){
@@ -316,10 +397,11 @@ int main(int args, char **argv){
 
 	sprintf(line, "nume fisier: %s\n", f->d_name);
 	write_file();
+        written_lines++;
 	reg = 1;
-	info_files(reg, buff.st_size, user_id, time, no_links, access);
+	info_files(reg, buff.st_size, user_id, time, no_links, access, &written_lines);
 
-	exit(EXIT_SUCCESS);
+	exit(written_lines);
       }
 
       waiting(id_reg);
@@ -328,22 +410,29 @@ int main(int args, char **argv){
 
     else if(is_dir(buff)){
       pid_t id_dir = fork();
+     
       if(id_dir < 0){
 	perror("Error fork directory");
 	exit(EXIT_FAILURE);
       }
       
       if(id_dir == 0){
+	
 	statistica(argv[2], f->d_name);
+
 	sprintf(line, "nume director: %s\n", f->d_name);
 	write_file();
+	written_lines++;
+	
+	info_files(reg, buff.st_size, user_id, time, no_links, access, &written_lines);
+	printf("%d\n", written_lines);
+	
+        exit(written_lines);
 
-	info_files(reg, buff.st_size, user_id, time, no_links, access);
-
-	exit(EXIT_SUCCESS);
       }
       
       waiting(id_dir);
+      
     }
 
     else if(is_link(buff)){
@@ -356,11 +445,10 @@ int main(int args, char **argv){
       if(link_id == 0){
 	statistica(argv[2], f->d_name);
 
-        info_symlink(f->d_name, buff.st_size, access);
+        info_symlink(f->d_name, buff.st_size, access, &written_lines);
 
-	exit(EXIT_SUCCESS);
+	exit(written_lines);
       }
-
       waiting(link_id);
     }
   }
